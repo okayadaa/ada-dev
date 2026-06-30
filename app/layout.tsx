c1ec998deeb2532a38b5543905e23d31146e5177
Adamarys Muniz <ADAMARYS.MUNIZ-SANCHEZ92@myhunter.cuny.edu>
2026-06-30 15:45:42 -0400

    Adjusted the title name

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Ada Dev",
+  title: "Welcome to my site!",
   icons: {
     icon: [{ url: "/seed-germination.jpg", type: "image/jpeg" }],
   },
